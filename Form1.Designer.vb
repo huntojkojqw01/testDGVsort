8c04e992df9891e8b21de8b66ab8c2516b9ae4a3
Cao thu vo lam <hungnd.k58@gmail.com>
2018-08-24 18:25:29 +0700

    init

---
+++ Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DateTime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.DateTime})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(593, 300)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Column3"
+        Me.Column3.Name = "Column3"
+        '
+        'DateTime
+        '
+        Me.DateTime.HeaderText = "Datetime"
+        Me.DateTime.Name = "DateTime"
+        Me.DateTime.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(697, 13)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Col1Sort"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(697, 58)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Col2Sort"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(671, 253)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 59)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Load"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(697, 116)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(120, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Col2,Col1Sort"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(697, 159)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(107, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "DatetimeSort"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(697, 200)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(107, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Col2, Datetime Sort"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(949, 324)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents DateTime As DataGridViewTextBoxColumn
+    Friend WithEvents Button6 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,82 @@
+﻿Public Class Form1
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Row As DataGridViewRow = Nothing
+        Dim rowIndex As Integer 'index of the row
+        Me.DataGridView1.Rows.Clear()
+
+        For i As Integer = 0 To 10
+            '/////Create a new row and get its index/////
+            rowIndex = Me.DataGridView1.Rows.Add()
+
+            '//////Get a reference to the new row ///////
+            Row = Me.DataGridView1.Rows(rowIndex)
+
+            With Row
+                'This won't fail since the columns exist 
+                .Cells("Column1").Value = "col1-" & rowIndex
+                If i Mod 3 = 0 Then
+                    .Cells("Column2").Value = "000"
+                Else
+                    .Cells("Column2").Value = "col2-" & (10 - rowIndex)
+                End If
+
+                If i Mod 4 = 0 Then
+                    .Cells("Column3").Value = "111"
+                Else
+                    .Cells("Column3").Value = "col3-" & (10 * rowIndex)
+                End If
+                .Cells("Datetime").Value = Now.AddHours(rowIndex * 12.5)
+            End With
+
+            '//// I think this line is not required 
+            ' Me.ServiceOrdersDataGridView.Rows.Add() 
+        Next
+        'DataGridView1.Rows.Add(row)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        DataGridView1.Sort(DataGridView1.Columns("Column1"), System.ComponentModel.ListSortDirection.Ascending)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        DataGridView1.Sort(DataGridView1.Columns("Column2"), System.ComponentModel.ListSortDirection.Ascending)
+    End Sub
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        DataGridView1.Sort(New RowComparer)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        DataGridView1.Sort(DataGridView1.Columns(3), System.ComponentModel.ListSortDirection.Ascending)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        DataGridView1.Sort(New RowComparer2)
+    End Sub
+End Class
+Public Class RowComparer
+    Implements IComparer
+    Public Function Compare(ByVal x As Object, ByVal y As Object) As Integer Implements System.Collections.IComparer.Compare
+        Dim DataGridViewRow1 As DataGridViewRow = CType(x, DataGridViewRow)
+        Dim DataGridViewRow2 As DataGridViewRow = CType(y, DataGridViewRow)
+        Dim CompareResult As Integer = System.String.Compare(DataGridViewRow1.Cells(1).Value.ToString(), DataGridViewRow2.Cells(1).Value.ToString())
+        If CompareResult = 0 Then
+            CompareResult = System.String.Compare(DataGridViewRow1.Cells(0).Value.ToString(), DataGridViewRow2.Cells(0).Value.ToString())
+        End If
+        Return CompareResult
+    End Function
+End Class
+Public Class RowComparer2
+    Implements IComparer
+    Public Function Compare(ByVal x As Object, ByVal y As Object) As Integer Implements System.Collections.IComparer.Compare
+        Dim DataGridViewRow1 As DataGridViewRow = CType(x, DataGridViewRow)
+        Dim DataGridViewRow2 As DataGridViewRow = CType(y, DataGridViewRow)
+        Dim CompareResult As Integer = System.String.Compare(DataGridViewRow1.Cells(1).Value.ToString(), DataGridViewRow2.Cells(1).Value.ToString())
+        If CompareResult = 0 Then
+            CompareResult = System.DateTime.Compare(DataGridViewRow1.Cells(3).Value, DataGridViewRow2.Cells(3).Value)
+        End If
+        Return CompareResult
+    End Function
+End Class
